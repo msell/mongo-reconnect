07fc0d8d2d5cb70a7a78bd3a4a882031e22adc45
Matt Sell <mwsell@gmail.com>
2015-01-06 20:47:42 -0600

    got a couple working tests

--- server.js
+++ server.js
@@ -4,13 +4,17 @@ var mongoose = require('mongoose');
 var Widget = require('./models/Widget.js');
 var app = express();
 
-app.use(bodyParser.json());
+app.use(bodyParser());
 
 
-app.get('/widget', function (req, res) {
+app.get('/widget', function (req, res, next) {
     res.send(200, "ok");
+    next();
 })
 
+app.post('/widget', function(req, res){
+    res.json({'name':'foo'});    
+})
 
 mongoose.connect('mongodb://localhost/reconnect-issue');
 
--- test/lostDbConnectionSpec.js
+++ test/lostDbConnectionSpec.js
@@ -1,7 +1,50 @@
+var express = require('express');
 var request = require('supertest');
 var chai = require('chai');
 var expect = chai.expect;
+var app = {};
+var bodyParser = require('body-parser');
 
-describe('sunny day', function(){
-    it('should be able to get widgets');
-})
+
+describe('sunny day', function () {
+    beforeEach(function () {
+        app = express();
+        app.use(bodyParser.json());
+
+        app.post('/widget', function (req, res) {
+            res.json({
+                'name': 'foo'
+            });
+        })
+        
+        app.get('/widget', function (req, res, next) {
+            res.status(200).send('ok');
+            next();
+        })       
+    });
+
+        it('should be able to add widgets', function (done) {
+            request(app).post('/widget')
+                .send({
+                    'name': 'foo'
+                })
+                .expect(200)
+                .end(function (err, res) {
+                    if (err) return done(err);
+                    done();
+                });
+        })
+    it('should be able to get widgets', function (done) {
+
+        request(app).get('/widget')
+            .expect(200)
+            .end(function (err, res) {
+                if (err) {
+                    console.log(err);
+                    return done(err);
+                }
+                done();
+            });
+
+    });
+});
