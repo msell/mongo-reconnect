e9308c796a82306fbafe4763fbadf8d2d1763331
Matt Sell <mwsell@gmail.com>
2015-02-01 09:34:03 -0600

    return if timeout occurs

--- app.js
+++ app.js
@@ -7,17 +7,14 @@ var timeout = require('connect-timeout');
 var app = express();
 
 app.use(bodyParser.json());
-app.use(timeout(2000));
+app.use(timeout('5s'));
 app.use(haltOnTimedout);
-
-function haltOnTimedout(req, res, next){
-  if (!req.timedout) next();
-}
 
 app.get('/widget', function (req, res, next) {
     Widget.find(function (err, widgets) {
         if (err) return console.error(err);
-
+        if (req.timedout) return;
+        
         res.status(200).send(widgets);
         next();
     })
@@ -27,12 +24,16 @@ app.post('/widget', function (req, res) {
     var widget = new Widget(req.body);
     widget.save(function (err, widget) {
         if (err) return console.error(err);
-
+        if (req.timedout) return;
+        
         console.log('saved widget ' + widget);
     })
     res.json(res.body);
 })
 
+function haltOnTimedout(req, res, next){
+  if (!req.timedout) next();
+}
 
 mongoose.connect();
 module.exports = app;
